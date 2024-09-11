12ae320e01b1a78370aad0e755f67166c591ca5f
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 20:04:39 +0100

    completed program

--- Day 3 - ReturnCommand(char-stat-gen).py
+++ Day 3 - ReturnCommand(char-stat-gen).py
@@ -1,16 +1,21 @@
 import random
+from itertools import count
+
 print('🪓 CHARACTER STAT GENERATOR 🪓\n')
 
-warriorName = input('Name your warrior: ')
-
+charNumber = int(input('How many characters : '))
+count = charNumber
 
 def generateHealth():
+    warriorName = input('Name your warrior: ')
     sidedRoll6 = random.randint(1,6)
     sidedRoll8 = random.randint(1, 8)
 
     rollMultiplication =  sidedRoll8 * sidedRoll6
+    return (f"Their health is {rollMultiplication}hp\n")
 
-    return f"Their health is {rollMultiplication}hp"
+while count > 0:
+    warr = generateHealth()
+    print(warr)
+    count-=1
 
-generateHealth()
-print(generateHealth())
