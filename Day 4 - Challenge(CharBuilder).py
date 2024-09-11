7afa3cd2d30e08de358172ccd4dd7fef48683dce
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 20:38:05 +0100

    Completed Program

--- Day 4 - Challenge(CharBuilder).py
+++ Day 4 - Challenge(CharBuilder).py
@@ -1,1 +1,30 @@
-print("🔨 CHARACTER BUILDER 🔨")
+import random, time, os
+print("🔨 CHARACTER BUILDER 🔨\n")
+
+def mainGame():
+    charName = input("Name your Legend: \n")
+    charType = input("Character Type(Human, Elf, Wizard, Orc): \n")
+    print(f'\n{charName}')
+    healthStat = ((random.randint(1,6) * random.randint(1,12)) / 2) + 10
+    strengthStat = ((random.randint(1,6) * random.randint(1,8)) / 2) + 10
+
+    time.sleep(0.7)
+    return (f'HEALTH: {healthStat}\nSTRENGTH: {strengthStat} \n\nMay your name go down in Legend..\n')
+
+    time.sleep(0.7)
+
+while True:
+    run = mainGame()
+    print(run)
+    time.sleep(1)
+    menu = input("'Yes' or 'No', Again?:\n")
+    if menu.lower() == 'yes':
+        os.system('cls')
+        time.sleep(1)
+    else:
+        break
+
+
+
+
+
