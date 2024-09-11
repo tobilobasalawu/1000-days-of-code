a80dd32a4309e06dadb4bb69ddd521be8b97a088
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 17:19:50 +0100

    multplication value input

--- Day 1 - Challenge.py
+++ Day 1 - Challenge.py
@@ -1,1 +1,14 @@
-print('yes')
+import time
+print('Math Game!')
+
+userInput = int(input("Name your multiples: "))
+score = 0
+
+def runGame():
+    for i in range(1, 4):
+        valueInput = int(input(f"{i} x {userInput} = "))
+        if valueInput == i * userInput:
+            print('Great Work! 🥳')
+
+
+runGame()
