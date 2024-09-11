c499f05630d5d283bb78c485f832c2353f80fa2e
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 17:41:59 +0100

    Completed challenge

--- Day 1 - Challenge.py
+++ Day 1 - Challenge.py
@@ -1,19 +1,21 @@
 import time
-print('Math Game!')
+import random
+print('Math Game!\n\n')
 
 userInput = int(input("Name your multiples: "))
 
 def runGame():
     score = 0
+    correctStatement = ["Great Work! 🥳", "Very Demure!💅🏾"]
     for i in range(1, 11):
-        valueInput = int(input(f"{i} x {userInput} = \n"))
+        valueInput = int(input(f"\n{i} x {userInput} = \n"))
         if valueInput == i * userInput:
             score += 1
-            print('Great Work! 🥳' or 'Flex!💅🏾')
+            print(random.choice(correctStatement))
         else:
             print(f'Sike! 😂 The answer is {i * userInput}')
-    print('. . .')
+    print('\n. . .')
     time.sleep(1)
-    print(f'\nYou scored {score} out of 10')
+    print(f'\nYou scored {score} out of 10!')
 
 runGame()
