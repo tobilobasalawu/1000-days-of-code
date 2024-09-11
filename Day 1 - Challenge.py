70b229be9a071b93744bafb723122c6523c9cd0d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 17:35:15 +0100

    Displaying final statement

--- Day 1 - Challenge.py
+++ Day 1 - Challenge.py
@@ -4,14 +4,16 @@ print('Math Game!')
 userInput = int(input("Name your multiples: "))
 
 def runGame():
+    score = 0
     for i in range(1, 11):
         valueInput = int(input(f"{i} x {userInput} = \n"))
-        score = 0
         if valueInput == i * userInput:
             score += 1
             print('Great Work! 🥳' or 'Flex!💅🏾')
         else:
             print(f'Sike! 😂 The answer is {i * userInput}')
-
+    print('. . .')
+    time.sleep(1)
+    print(f'\nYou scored {score} out of 10')
 
 runGame()
