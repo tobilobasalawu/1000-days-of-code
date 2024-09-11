afe9e8e43ed4ff954f9d15d5fdb5470b8645888b
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 17:56:57 +0100

    Declared and assigned details

--- Day 2 - Subroutine(loginSystem).py
+++ Day 2 - Subroutine(loginSystem).py
@@ -1,1 +1,9 @@
 print('REPLIT LOGIN SYSTEM!')
+
+savedUsername = 'tobilobasalawu'
+savedPassword = 'A2gaut1503'
+
+userName = input('What is your username: ')
+password = input('What is your password: ')
+
+
