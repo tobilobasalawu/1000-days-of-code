2ed77c38881a8094cbf28931aba266c69e77ecfe
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 19:05:49 +0100

    generated 6-sided-roll & 8-sided-roll

--- Day 3 - ReturnCommand(char-stat-gen).py
+++ Day 3 - ReturnCommand(char-stat-gen).py
@@ -1,1 +1,12 @@
-print('🪓 CHARACTER STAT GENERATOR 🪓')
+import random
+print('🪓 CHARACTER STAT GENERATOR 🪓\n')
+
+warriorName = input('Name your warrior: ')
+
+
+def generateHealth():
+    sidedRoll6 = random.randint(1,6)
+    sidedRoll8 = random.randint(1, 8)
+
+generateHealth()
+
