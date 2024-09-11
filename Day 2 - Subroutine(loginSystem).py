1663a5cc33b6ed90549651942b5c8e920f5b0c95
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-11 18:10:36 +0100

    verifying details section

--- Day 2 - Subroutine(loginSystem).py
+++ Day 2 - Subroutine(loginSystem).py
@@ -3,7 +3,14 @@ print('REPLIT LOGIN SYSTEM!')
 savedUsername = 'tobilobasalawu'
 savedPassword = 'A2gaut1503'
 
-userName = input('What is your username: ')
-password = input('What is your password: ')
+def loginSystem():
+    while True:
+        userName = input('What is your username: ')
+        password = input('What is your password: ')
+        if userName != savedUsername and password != savedPassword:
+            print('Try Again')
+        else:
+            print('Welcome to KALI🐍')
+            break
 
-
+loginSystem()
