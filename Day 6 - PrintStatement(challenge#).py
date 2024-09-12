bb85fde7d0f8f375765e198dcb94bca5c5369a69
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-12 18:09:06 +0100

    print statement tricks

--- Day 6 - PrintStatement(challenge#).py
+++ Day 6 - PrintStatement(challenge#).py
@@ -1,2 +1,18 @@
+'''
 for i in range(0, 100):
     print(i, end = '')
+
+
+import os
+import time
+
+print('\033[?25l', end="")
+
+for i in range(1, 101):
+    print(i)
+    time.sleep(0.1)
+    os.system('cls')
+
+print('\033[?25h', end="")
+
+'''
