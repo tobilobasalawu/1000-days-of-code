ea02091c198bb33882cef4cddba5648964612e4c
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-12 17:54:31 +0100

    Failed Program

--- Day 5 - Challenge(CharBuilder2).py
+++ Day 5 - Challenge(CharBuilder2).py
@@ -25,12 +25,15 @@ def runGame():
 
     print('The battle begins!\n')
 
-    roundWin1 = random.randint(1,6)
-    roundWin2 = random.randint(1,6)
-    diffStr = strstat2-strstat1
-    if roundWin1 > roundWin2:
-        print(f'{charName1} wins the first blow\n{charName2} takes a hit, with {abs(diffStr) + 1} damage')
-    else:
-        print(f'{charName2} wins the first blow\n{charName1} takes a hit, with {abs(diffStr) + 1} damage')
-
+    while healthstat2 > 0 or healthstat1 > 0:
+        roundWin1 = random.randint(1,6)
+        roundWin2 = random.randint(1,6)
+        diffStr = strstat2-strstat1
+        if roundWin1 > roundWin2:
+            print(f'{charName1} wins the blow\n{charName2} takes a hit, with {abs(diffStr) + 1} damage')
+            print(f'{charName1}\nHEALTH:{healthstat1-diffStr}\n\n{charName2}\nHEALTH:{healthstat2}')
+        else:
+            print(f'{charName2} wins the blow\n{charName1} takes a hit, with {abs(diffStr) + 1} damage')
+            print(f'{charName1}\nHEALTH:{healthstat1}\n\n{charName2}\nHEALTH:{healthstat2-diffStr}')
+            break
 runGame()
