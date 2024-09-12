5e0df5f0d749684c6b7b4eee316f621b6b65a6f6
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-12 16:25:20 +0100

    set up health and str stats

--- Day 5 - Challenge(CharBuilder2).py
+++ Day 5 - Challenge(CharBuilder2).py
@@ -1,9 +1,24 @@
-print('🔪 BATTLE TIME 🔪\n')
-
-charName = input("Name your Legend:\n")
-charType = input("Character Type (Human, Elf, Wizard, Orc)):\n")
+import random, time
+print('🔪 BATTLE TIME 🔪')
 
 def runGame():
-    print(f'\n{charName}')
+    #print('Who are they battling ?')
+    fighters = 2
+
+    healthstat = ((random.randint(1,6) * random.randint(1, 12))/2) + 10
+    strstat = ((random.randint(1,6) * random.randint(1, 8))/2) + 10
+    while fighters > 0:
+        if fighters == 1:
+            print('\nWho are they battling?')
+        print('')
+        charName = input("Name your Legend:\n")
+        charType = input("Character Type (Human, Elf, Wizard, Orc)):\n")
+
+        print(f'\n{charName}\nHEALTH: {healthstat}\nSTRENGTH: {strstat}')
+        fighters-=1
+
+
+
+
 
 runGame()
