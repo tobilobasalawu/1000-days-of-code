19e256cf721738354adcb74fcf2f5caa13d378e0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-12 16:03:19 +0100

    Setting up questions for users

--- Day 5 - Challenge(CharBuilder2).py
+++ Day 5 - Challenge(CharBuilder2).py
@@ -1,2 +1,9 @@
-print('🔪 BATTLE TIME 🔪')
+print('🔪 BATTLE TIME 🔪\n')
 
+charName = input("Name your Legend:\n")
+charType = input("Character Type (Human, Elf, Wizard, Orc)):\n")
+
+def runGame():
+    print(f'\n{charName}')
+
+runGame()
