4e88e1714c930b4c9f7d9ac5b33a085780f35912
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-13 15:17:05 +0100

    Improved code

--- Day 8 - fStrings(Challenge).py
+++ Day 8 - fStrings(Challenge).py
@@ -2,4 +2,6 @@ print('30 Days Down - What do you think?/n')
 
 for i in range(1,31):
     askUser = input(f'\nDay {i}:\n')
-    print(f'\n      You thought Day {i} was \n            {askUser}')
+    sysText = f'You Thought Day {i} was'
+    print(f'{sysText:^50}', end="")
+    print(f'\n{askUser: ^50}')
