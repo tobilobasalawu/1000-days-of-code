79aff05238ddb734e2b8bd7f4f577b63f337d197
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-13 15:07:03 +0100

    Adding loops and input questions

--- Day 8 - fStrings(Challenge).py
+++ Day 8 - fStrings(Challenge).py
@@ -1,1 +1,5 @@
-print('30 Days Down - What do you think?')
+print('30 Days Down - What do you think?/n')
+
+for i in range(1,31):
+    askUser = input(f'Day {i}:\n')
+    print(f'\n You thought Day {i} was {askUser}')
