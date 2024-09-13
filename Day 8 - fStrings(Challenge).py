beffff81e6582d6b11389511daf4628cf9866e4d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-13 15:10:15 +0100

    Alignment and spacing

--- Day 8 - fStrings(Challenge).py
+++ Day 8 - fStrings(Challenge).py
@@ -2,4 +2,4 @@ print('30 Days Down - What do you think?/n')
 
 for i in range(1,31):
     askUser = input(f'Day {i}:\n')
-    print(f'\n You thought Day {i} was {askUser}')
+    print(f'\n      You thought Day {i} was \n            {askUser}')
