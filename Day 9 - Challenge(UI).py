d3ccc7379aa839561405ab623f5da832d2a216b7
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-13 15:51:20 +0100

    Completed Program

--- Day 9 - Challenge(UI).py
+++ Day 9 - Challenge(UI).py
@@ -1,5 +1,6 @@
 title = ' Music App '
-print('\033[31m', '=', '\033[0m', '=', '\033[34m', '=', '\033[0m', title,'\033[34m', '=', '\033[0m', '=', '\033[31m', '=\n' , sep="", end="")
+empt = ''
+print(f'{empt:^20}\033[31m=\033[0m=\033[34m=\033[0m{title}\033[34m=\033[0m=\033[31m=\n' , sep="", end="")
 songName = 'Radio Gaga'
 name = 'Queen'
 print(f'\n🔥▶\033[0m{songName:^14}')
