34e91cac0ce3b864c67d8f17c395bc87e24d3a6d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-13 15:35:36 +0100

    UI Title

--- Day 9 - Challenge(UI).py
+++ Day 9 - Challenge(UI).py
@@ -1,2 +1,5 @@
-title = 'Music App'
-print('\033[31m', title, end="")
+title = ' Music App '
+print('\033[31m', '=', '\033[0m', '=', '\033[34m', '=', '\033[0m', title,'\033[34m', '=', '\033[0m', '=', '\033[31m', '=\n' , sep="", end="")
+
+
+
