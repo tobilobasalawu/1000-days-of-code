b6ee33f6c2e233f0a8e2b23f258b77d94095fefe
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-14 11:43:50 +0100

    Added the edit feature

--- Day 10 - Lists.py
+++ Day 10 - Lists.py
@@ -1,12 +1,26 @@
 print('ToDo List Manager')
 
-task = ['yes', 'no']
 def toDo():
+    task = ['test','yes']
     userInput = input('Do you want to view, add or edit the todo list?\n')
     userInput = userInput.lower()
     print('')
 
-    if userInput == 'view':
+    if userInput == 'add':
+        userAdd = input('What task do you want to add:\n')
+        task.append(userAdd)
         for i in task:
             print(f'{i}')
+
+    elif userInput == 'view':
+        for i in task:
+            print(f'{i}')
+
+    elif userInput == 'edit':
+        editInput = input('What task do you want to remove:\n')
+        task.remove(editInput.lower())
+        for i in task:
+            print(f'\n{i}')
+
+
 toDo()
