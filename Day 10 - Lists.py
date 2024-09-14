def4a9d10a29f927d3a851309d965e70752a0339
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-14 11:54:37 +0100

    Adding loops

--- Day 10 - Lists.py
+++ Day 10 - Lists.py
@@ -1,26 +1,33 @@
 print('ToDo List Manager')
+task = []
 
 def toDo():
-    task = ['test','yes']
-    userInput = input('Do you want to view, add or edit the todo list?\n')
-    userInput = userInput.lower()
-    print('')
+    while True:
+        userInput = input('\nDo you want to view, add or edit the todo list?\n')
+        userInput = userInput.lower()
+        print('')
 
-    if userInput == 'add':
-        userAdd = input('What task do you want to add:\n')
-        task.append(userAdd)
-        for i in task:
-            print(f'{i}')
+        if userInput == 'add':
+            userAdd = input('What task do you want to add:\n')
+            task.append(userAdd)
+            print('\nTask Added🎉')
+            for i in task:
+                print(f'{i}')
 
-    elif userInput == 'view':
-        for i in task:
-            print(f'{i}')
+        elif userInput == 'view':
+            for i in task:
+                if i == None:
+                    print('No Tasks Available')
+                print(f'{i}')
 
-    elif userInput == 'edit':
-        editInput = input('What task do you want to remove:\n')
-        task.remove(editInput.lower())
-        for i in task:
-            print(f'\n{i}')
 
+        elif userInput == 'edit':
+            editInput = input('What task do you want to remove:\n')
+            task.remove(editInput.lower())
+            for i in task:
+                print(f'{i}')
+
+        else:
+            print('\nInvalid Input, Select the Correct Option!')
 
 toDo()
