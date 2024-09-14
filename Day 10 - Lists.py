67c73b0aec90490b73cac6a6b42ddeceb38722e9
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-14 11:28:54 +0100

    Added the view feature

--- Day 10 - Lists.py
+++ Day 10 - Lists.py
@@ -1,7 +1,12 @@
 print('ToDo List Manager')
 
-task = []
+task = ['yes', 'no']
 def toDo():
     userInput = input('Do you want to view, add or edit the todo list?\n')
+    userInput = userInput.lower()
+    print('')
 
+    if userInput == 'view':
+        for i in task:
+            print(f'{i}')
 toDo()
