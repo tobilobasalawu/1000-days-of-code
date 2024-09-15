3b438798642ac91d5b20f706467b19c01d8311b0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-15 21:39:34 +0100

    adding guessedwords and wordlist

--- Day 13 - Hangman!.py
+++ Day 13 - Hangman!.py
@@ -3,7 +3,15 @@ from urllib.parse import uses_netloc
 
 print('HANGMAN GAME💅🏾\n')
 
-Wordlist = 'British'
+wordlist = 'British'
 guessedWord = []
 lives = 6
+
+def runGame():
+    while lives > 0:
+        for i in wordlist:
+            if i in guessedWord:
+                print(i, end=" ")
+            else:
+                print('_', ' ')
 
