634c45a1fa16544f3d92d54936a230847212a540
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-15 16:05:58 +0100

    returning the result for words found

--- Day 13 - Hangman!.py
+++ Day 13 - Hangman!.py
@@ -1,5 +1,14 @@
 import random
+from urllib.parse import uses_netloc
+
 print('HANGMAN GAME💅🏾\n')
 
 Wordlist = ['Telephone', 'British', 'Chat', 'Evil']
 randomWords = random.choice(Wordlist)
+print(randomWords)
+
+userInput = input('Choose a Letter: ')
+
+if userInput in randomWords:
+    print('You found a letter')
+
