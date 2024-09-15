04d0115039e542b1faead7900672f89792e06f5f
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-15 21:34:53 +0100

    restructuring the program

--- Day 13 - Hangman!.py
+++ Day 13 - Hangman!.py
@@ -3,24 +3,7 @@ from urllib.parse import uses_netloc
 
 print('HANGMAN GAME💅🏾\n')
 
-Wordlist = ['Telephone', 'British', 'Chat', 'Evil']
-randomWords = random.choice(Wordlist)
-print(randomWords)
+Wordlist = 'British'
+guessedWord = []
+lives = 6
 
-letterPrint = []
-lives = 5
-
-def runGame():
-    while True:
-        userInput = input('Choose a Letter: ')
-        if userInput in letterPrint:
-            print('You have already chosen the letter')
-            continue
-
-        if userInput in randomWords:
-            print('You found a letter')
-            letterPrint.append(userInput)
-        else:
-            print('Nope, not in there')
-
-runGame()
