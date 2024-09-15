55fe107d3cfcc5854a71b9ec3cccf1996d32d8c4
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-15 22:19:34 +0100

    added time intervals

--- Day 13 - Hangman!.py
+++ Day 13 - Hangman!.py
@@ -3,7 +3,7 @@ import time
 
 print('HANGMAN GAME💅')
 
-wordlist = 'intelligence'
+wordlist = 'Television'
 guessedWord = []
 
 def runGame():
@@ -11,14 +11,17 @@ def runGame():
     while lives > 0:
         for i in wordlist:
             if i in guessedWord:
+                time.sleep(0.6)
                 print(i, end=" ")
             else:
+                time.sleep(0.1)
                 print('_', end=' ')
         print()
 
         userInput = input("\nEnter a word:\n").lower()
 
         if userInput in guessedWord:
+            time.sleep(0.8)
             print(f"\nYou already guessed the letter '{userInput}'")
 
         elif userInput in wordlist:
@@ -27,6 +30,7 @@ def runGame():
         else:
             print('\nWrong Guess!')
             lives -= 1
+            time.sleep(1)
             print(f'You have {lives} lives left')
 
         if set(guessedWord) == set(wordlist):
@@ -34,7 +38,11 @@ def runGame():
             break
 
     if lives == 0:
-        print(f'\nYou Lost 😪, the word is {wordlist.upper()}')
+        print(f'\nYou Lost 😪')
+        time.sleep(0.3)
+        print(f"The word is", end=" ")
+        time.sleep(0.8)
+        print(f"'{wordlist.upper()}'")
 
 
 runGame()
