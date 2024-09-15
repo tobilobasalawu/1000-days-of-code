50ad1c158b0fd77415b1c51569393f742830c6a2
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-15 16:26:22 +0100

    returning the result for previously entered letters

--- Day 13 - Hangman!.py
+++ Day 13 - Hangman!.py
@@ -7,15 +7,20 @@ Wordlist = ['Telephone', 'British', 'Chat', 'Evil']
 randomWords = random.choice(Wordlist)
 print(randomWords)
 
+letterPrint = []
 lives = 5
 
 def runGame():
     while True:
         userInput = input('Choose a Letter: ')
+        if userInput in letterPrint:
+            print('You have already chosen the letter')
+            continue
+
         if userInput in randomWords:
             print('You found a letter')
+            letterPrint.append(userInput)
         else:
             print('Nope, not in there')
-
 
 runGame()
