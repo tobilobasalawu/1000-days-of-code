a0bdc2cafeadf15c1565d36f46377052a77af5a9
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-17 21:42:40 +0100

    looping each item in the dictionary

--- Day 15 - MokeBeast(challenge).py
+++ Day 15 - MokeBeast(challenge).py
@@ -3,8 +3,11 @@ print('MokéBeast')
 beastName = input("Beast Name: ")
 beastType = input("Type: ")
 specialMove = input("Special Move: ")
-hp = input("HP: ")
-mp = input("MP: ")
+hp = int(input("HP: "))
+mp = int(input("MP: "))
 
-beast = {"Beast Name: " : beastName, 'Type: ' : beastType, 'Special Move: ' : specialMove, 'HP: ' : hp, 'MP: ': mp}
+print()
+beast = {"Beast Name" : beastName, 'Type' : beastType, 'Special Move' : specialMove, 'HP' : hp, 'MP': mp}
 
+for key,values in beast.items():
+    print(f'{key}:  {values}')
