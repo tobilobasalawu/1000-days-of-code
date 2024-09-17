c7d5a3e0a96643c8008da72fc405d90f45cde42a
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-17 19:08:11 +0100

    starting challenge

--- Day 14 - Dictionaries.py
+++ Day 14 - Dictionaries.py
@@ -1,4 +1,9 @@
+'''
 myDictionary = {"names" : "Ian", "health" : 219, "strength" : 199, "equipped" : "Axe"}
 
 for name, values in myDictionary.items():
     print(f"{name} : {values}")
+
+'''
+
+example = {"myValue" : None}
