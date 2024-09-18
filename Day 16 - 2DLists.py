a8f4279921c662417a4c0b7e178cae957e0c732c
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-18 17:42:18 +0100

    Starting challenge

--- Day 16 - 2DLists.py
+++ Day 16 - 2DLists.py
@@ -1,8 +1,13 @@
+'''
 my2DList = [
     ['Jonny', 10, 'Mac'],
     ['Sian', 19, 'PC'],
     ['Jonny', 10, 'Mac']
 ]
 
-newLists = my2DList[0][1]
-print(newLists)
+my2DList[0][2] = 'Linux'
+print(my2DList[0])
+
+'''
+
+print('BINGO CARD')
