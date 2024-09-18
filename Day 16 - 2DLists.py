4190336a349f2b57476ef676afa265a85823a786
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-18 17:51:40 +0100

    randomly generating the number

--- Day 16 - 2DLists.py
+++ Day 16 - 2DLists.py
@@ -12,3 +12,13 @@ print(my2DList[0])
 
 import random
 print("David's Nan's Bingo Card Generator")
+print()
+
+number = random.randint(1, 90)
+numbers = []
+
+for i in range(8):
+    numbers.append(number)
+numbers.sort()
+
+print()
