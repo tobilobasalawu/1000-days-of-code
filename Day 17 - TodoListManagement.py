e70328668085f5b4cf1764ae607e308f52961c33
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-19 19:09:02 +0100

    adding each task list to the todolist

--- Day 17 - TodoListManagement.py
+++ Day 17 - TodoListManagement.py
@@ -1,9 +1,18 @@
 print("TODO LIST MANAGEMENT APP\n")
 
-menu = input("1: Add\n2: View\n3: Remove\n4: Edit\n\n>")
+menu = input("1: Add\n2: View\n3: Remove\n4: Edit\n\n> ")
+todoList = []
 
 def todoProgram():
     if menu.lower() == 'add' or '1':
+        print('ADD')
         task = input("\nWhat is it? ")
-        taskTime = input("\nWhat is it due? ")
-        taskPriority = input("\nHow Important? ")
+        taskTime = input("What is it due? ")
+        taskPriority = input("How Important? ")
+        row = [task, taskTime, taskPriority]
+
+        todoList.append(row)
+        print(todoList)
+
+
+todoProgram()
