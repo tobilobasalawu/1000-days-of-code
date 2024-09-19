a55a6d163f6f6944a619e84b12369ade465e83eb
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-19 19:12:08 +0100

    a submenu for the view section

--- Day 17 - TodoListManagement.py
+++ Day 17 - TodoListManagement.py
@@ -4,15 +4,17 @@ menu = input("1: Add\n2: View\n3: Remove\n4: Edit\n\n> ")
 todoList = []
 
 def todoProgram():
-    if menu.lower() == 'add' or '1':
+    if menu.lower() == 'add':
         print('ADD')
         task = input("\nWhat is it? ")
         taskTime = input("What is it due? ")
         taskPriority = input("How Important? ")
         row = [task, taskTime, taskPriority]
-
         todoList.append(row)
-        print(todoList)
-
+    elif menu.lower() == 'view':
+        print('VIEW')
+        viewMenu = int(input("\n1: View All\n2: View Priority\n\n> "))
+        if viewMenu == 1:
+            print(todoList)
 
 todoProgram()
