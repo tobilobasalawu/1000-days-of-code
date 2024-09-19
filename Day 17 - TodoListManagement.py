ceefbc156324e682925e6dc9907d4936916c1e9a
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-19 18:51:30 +0100

    a section for the add feature

--- Day 17 - TodoListManagement.py
+++ Day 17 - TodoListManagement.py
@@ -1,3 +1,8 @@
 print("TODO LIST MANAGEMENT APP\n")
 
 menu = input("1: Add\n2: View\n3: Remove\n4: Edit\n\n>")
+
+def todoProgram():
+    if menu.lower() == 'add' or '1':
+        task = input("\nWhat is it? ")
+        taskTime = input("\nWhat is it due? ")
