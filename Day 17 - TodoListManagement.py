e133869ec364f6fe2c3807d8ae07426497ed47e5
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-19 19:14:58 +0100

    Adding a loop to the program

--- Day 17 - TodoListManagement.py
+++ Day 17 - TodoListManagement.py
@@ -1,20 +1,20 @@
-print("TODO LIST MANAGEMENT APP\n")
-
-menu = input("1: Add\n2: View\n3: Remove\n4: Edit\n\n> ")
+print("TODO LIST MANAGEMENT APP")
 todoList = []
 
 def todoProgram():
-    if menu.lower() == 'add':
-        print('ADD')
-        task = input("\nWhat is it? ")
-        taskTime = input("What is it due? ")
-        taskPriority = input("How Important? ")
-        row = [task, taskTime, taskPriority]
-        todoList.append(row)
-    elif menu.lower() == 'view':
-        print('VIEW')
-        viewMenu = int(input("\n1: View All\n2: View Priority\n\n> "))
-        if viewMenu == 1:
-            print(todoList)
+    while True:
+        menu = input("\n1: Add\n2: View\n3: Remove\n4: Edit\n\n> ")
+        if menu.lower() == 'add':
+            print('ADD')
+            task = input("\nWhat is it? ")
+            taskTime = input("What is it due? ")
+            taskPriority = input("How Important? ")
+            row = [task, taskTime, taskPriority]
+            todoList.append(row)
+        elif menu.lower() == 'view':
+            print('VIEW')
+            viewMenu = int(input("\n1: View All\n2: View Priority\n\n> "))
+            if viewMenu == 1:
+                print(todoList)
 
 todoProgram()
