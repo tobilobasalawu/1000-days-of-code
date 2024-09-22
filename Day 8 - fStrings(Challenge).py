05602fde4240ccb8fe4a3a0bd71c4f78a396aa08
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-22 15:20:53 +0100

    updated files

--- Day 8 - fStrings(Challenge).py
+++
@@ -1,7 +0,0 @@
-print('30 Days Down - What do you think?/n')
-
-for i in range(1,31):
-    askUser = input(f'\nDay {i}:\n')
-    sysText = f'You Thought Day {i} was'
-    print(f'{sysText:^50}', end="")
-    print(f'\n{askUser: ^50}')
