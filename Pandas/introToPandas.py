decaffa018d40ed8f90f1860fbdcf0ea062fefaf
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-22 16:37:04 +0100

    reading data in text file

--- Pandas/introToPandas.py
+++ Pandas/introToPandas.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv(r"pokemon_data.csv")
-df_xlsx = pd.read_excel("pokemon_data.xlsx")
+#df = pd.read_csv(r"pokemon_data.csv")
+#df_xlsx = pd.read_excel("pokemon_data.xlsx")
 # print(df.tail(3)) Use tail to check bottom roles, use head for top rows
+
+df = pd.read_csv("pokemon_data.txt", delimiter="\t")
+print(df)
