8a8b8776d95bdafa269efb74ae2949e809c506bf
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-25 12:38:45 +0100

    completed program

--- codeWars/calType.py
+++ codeWars/calType.py
@@ -1,2 +1,15 @@
 def calc_type(a, b, res):
-    return # operation used
+    addition = a + b
+    subtraction = a - b
+    multiply = a * b
+    divide = a / b
+
+    if res == addition:
+        return 'addition'
+    elif res == subtraction:
+        return 'subtraction'
+    elif res == multiply:
+        return 'multiplication'
+    else:
+        return 'division'
+
