2b617723467bbddcddab8a255378ee385b31679d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-27 19:04:34 +0100

    errors in code

--- Hackerrank/diagonalDifference.py
+++ Hackerrank/diagonalDifference.py
@@ -3,3 +3,5 @@ def diagonalDifference(arr):
     rightD = arr[0][2] + arr[1][1] + arr[2][0]
 
     return abs(leftD - rightD)
+
+print(diagonalDifference([[-1,1,-7,-8], [-10, -8, -5, -2], [0, 9,7, -1], [4,4,-2,1]]))
