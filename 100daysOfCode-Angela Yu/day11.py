3608ab078b28488c71a3822ed76ed63fd19df919
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-28 15:43:10 +0100

    issues with solving problems

--- 100daysOfCode-Angela Yu/day11.py
+++ 100daysOfCode-Angela Yu/day11.py
@@ -1,37 +1,59 @@
 import random
 import sys
 
+
 def startGameInput():
-    startGame = input("Do you want to play the game of BlackJack? Type 'y' or 'n': ").lower()
-    if startGame == 'y':
-        return '🎴 BLACKJACK'
-    else:
-        return ''
-
-
-def gameAlogrithm():
     cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     userCards = []
     computerCards = []
 
-    userRandom = random.randint(0,10), random.randint(1,10)
-    for i in userRandom:
-        userCards.append(i)
+    r = random.randint(1,12)
+    deckRandom = random.randint(1,r)
+    userRandom = random.randint(0, 10), random.randint(1, 10)
+    computerRandom = random.randint(1, 10)
 
-    computerRandom = random.randint(0,10)
-    computerCards.append(computerRandom)
+    userScore = 0
+    computerScore = 0
+
+    blackJack = 21
+
+    startGame = input("Do you want to play the game of BlackJack? Type 'y' or 'n': ").lower()
+    if startGame == 'y':
+        print('🎴 BLACKJACK')
+        for i in userRandom:
+            userCards.append(i)
+        computerCards.append(computerRandom)
+
+        userScore = sum(userCards)
+        print(f"Your Cards: {userCards}, current score: {userScore}\n Computer's first Card: {computerCards}")
+
+        userInput = input("Type 'y' to get another card, type 'n' to pass: ")
+        if userInput.lower() == 'y':
+            userCards.append(deckRandom)
+            userScore = sum(userCards)
+            #computerScore = sum(computerCards)
+
+            print(f"Your Cards: {userCards}, current score: {userScore}\n Computer's Card: {computerCards}\n")
+
+        else:
+            if userScore > blackJack and computerScore == blackJack:
+                print(f"Your Cards: {userCards}, current score: {userScore}\n Computer's Card: {computerCards}, final Score:{computerScore}\nYou Lose!!")
+            elif userScore == blackJack and computerScore > blackJack:
+                print(f"Your Cards: {userCards}, current score: {userScore}\n Computer's Card: {computerCards}\nYou Win!!")
+            elif userScore < blackJack and computerScore < blackJack:
+                print(f"Your Cards: {userCards}, current score: {userScore}\n Computer's Card: {computerCards}\n{max(computerScore,userScore)}")
 
 
-    userScore = sum(userCards)
+
+    else:
+        print('')
 
 
-    return f"Your Cards: {userCards}, current score: {userScore}\n Computer's first Card: {computerCards}"
-
-print(gameAlogrithm())
+#startGameInput()
 
 
 
 
 
-#print(startGameInput())
+print(startGameInput())
 
