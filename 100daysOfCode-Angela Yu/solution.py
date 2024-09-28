df6a6e3521f1da5b08a16f43681ebfc5e90f2784
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-28 15:53:44 +0100

    hint2-dealing user and computer 2 cards each using deal_card()

--- 100daysOfCode-Angela Yu/solution.py
+++ 100daysOfCode-Angela Yu/solution.py
@@ -1,5 +1,12 @@
 import random
-def deal_card():
+def deal_card(): #returns random card from deck
     cards = [11, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+
+user_cards = []
+computer_cards = []
+
+for i in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
