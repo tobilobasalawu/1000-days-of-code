14270f23cb2f1508af9fec04c45b35bba32c8a8e
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-28 21:09:43 +0100

    completed program

--- 100daysOfCode-Angela Yu/Day14/myCode(highlower).py
+++ 100daysOfCode-Angela Yu/Day14/myCode(highlower).py
@@ -1,3 +1,46 @@
 from gameData import data
+from art import logo,vs
+import random
 
-print('Higher\n  Lower')
+def logoFunction():
+    print(logo)
+def vsFunction():
+    print(vs)
+
+
+def main():
+    score = 0
+    index = 0
+    index2 = 1
+
+    correct = True
+
+    randomA = data[index]
+    randomB = data[index2]
+
+    while correct:
+        A = f"Compare A: a {randomA['name']}, a {randomA['description']}, from {randomA['country']}.\n"
+        B = f"Against B: a {randomB['name']}, a {randomB['description']}, from {randomB['country']}."
+
+        logoFunction()
+        print(A)
+        vsFunction()
+        print(B)
+
+        higher = max(randomA['follower_count'],randomB['follower_count'])
+
+        userChoice = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if userChoice == 'a' and higher == randomA['follower_count']:
+            score+=1
+            index2 += 1
+            index += 1
+
+            randomA = data[index]
+            randomB = data[index2]
+            print(f"You're right! Current score: {score}")
+        else:
+            print(f"You're wrong, your final score: {score}")
+            break
+
+main()
+
