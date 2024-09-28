cfb73db2ea47f67e8ba570029c1430f3f3f5ea83
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-28 16:34:15 +0100

    add card to user existing card list

--- 100daysOfCode-Angela Yu/solution.py
+++ 100daysOfCode-Angela Yu/solution.py
@@ -6,6 +6,7 @@ def deal_card(): #returns random card from deck
 
 user_cards = []
 computer_cards = []
+game_over = False
 
 def calculate_score(cards): #calculating scores
     if sum(cards) == 21 and len(cards) ==2:
@@ -23,3 +24,14 @@ for i in range(2):
 
 user_score = calculate_score(user_cards)#calculating user score
 computer_score = calculate_score(computer_cards) #calculating computer score
+print(f"Your Cards: {user_cards}, current score: {user_score}\n Computer's Card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:#game ends if blackjack in user&computer or greater than 21
+    game_over = True
+else:
+    userInput = input("Type 'y' to get another card, type 'n' to pass: ")
+    if userInput == 'y':
+        user_cards.append(deal_card()) #if user selects another card, it should be added to current crad list
+    else:
+        game_over=True
+
