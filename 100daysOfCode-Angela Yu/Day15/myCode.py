8381491aac6742f9baa8a2977a456135dd9f01e0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-29 17:09:33 +0100

    set program into while loop

--- 100daysOfCode-Angela Yu/Day15/myCode.py
+++ 100daysOfCode-Angela Yu/Day15/myCode.py
@@ -3,24 +3,28 @@ defaultWater = MENU['default']['water']
 defaultMilk = MENU['default']['milk']
 defaultCoffee = MENU['default']['coffee']
 defaultMoney = MENU['default']['money']
+power = True
 
 def main():
-    userRequest = input("What would you like? (espresso/latte/cappuccino):\n").lower()
-    if userRequest == 'report':
-        print(f"Water: {MENU['default']['water']}ml\nMilk: {MENU['default']['milk']}ml\nCoffee: {MENU['default']['coffee']}g\nMoney: ${MENU['default']['money']}")
-    elif userRequest == 'latte':
-        if defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
-            print('Please Insert Coins')
-            quarterInput = int(input("how many quarters?: "))
-            dimeInput = int(input("how many dimes?: "))
-            nickleInput = int(input("how many nickles?: "))
-            penniesInput = int(input("how many pennies?: "))
-        elif defaultWater < MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
-            print('Sorry there is not enough water.')
-        elif defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk < MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
-            print('Sorry there is not enough milk.')
-        elif defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee < MENU['latte']['ingredients']['coffee']:
-            print('Sorry there is not enough coffee.')
+    while power:
+        userRequest = input("What would you like? (espresso/latte/cappuccino):\n").lower()
+        if userRequest == 'report':
+            print(f"Water: {MENU['default']['water']}ml\nMilk: {MENU['default']['milk']}ml\nCoffee: {MENU['default']['coffee']}g\nMoney: ${MENU['default']['money']}")
+        elif userRequest == 'latte':
+            if defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
+                print('Please Insert Coins')
+                quarterInput = int(input("how many quarters?: "))
+                dimeInput = int(input("how many dimes?: "))
+                nickleInput = int(input("how many nickles?: "))
+                penniesInput = int(input("how many pennies?: "))
+            elif defaultWater < MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
+                print('Sorry there is not enough water.')
+            elif defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk < MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
+                print('Sorry there is not enough milk.')
+            elif defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee < MENU['latte']['ingredients']['coffee']:
+                print('Sorry there is not enough coffee.')
+            else:
+                print('Sorry there is not enough resources')
 
 
 
