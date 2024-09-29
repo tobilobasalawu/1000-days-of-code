a3ef8ba5cfd102d2509711e5a1b0c81470b0b5f0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-29 16:52:11 +0100

    when user request is latte

--- 100daysOfCode-Angela Yu/Day15/myCode.py
+++ 100daysOfCode-Angela Yu/Day15/myCode.py
@@ -1,9 +1,16 @@
 from menuData import MENU
+defaultWater = MENU['default']['water']
+defaultMilk = MENU['default']['milk']
+defaultCoffee = MENU['default']['coffee']
+defaultMoney = MENU['default']['money']
 
 def main():
     userRequest = input("What would you like? (espresso/latte/cappuccino):\n").lower()
     if userRequest == 'report':
         print(f"Water: {MENU['default']['water']}ml\nMilk: {MENU['default']['milk']}ml\nCoffee: {MENU['default']['coffee']}g\nMoney: ${MENU['default']['money']}")
+    elif userRequest == 'latte':
+        if defaultWater > MENU['latte']['ingredients']['water'] and defaultMilk > MENU['latte']['ingredients']['milk'] and  defaultCoffee > MENU['latte']['ingredients']['coffee']:
+            print('Please Insert Coins')
 
 
 main()
