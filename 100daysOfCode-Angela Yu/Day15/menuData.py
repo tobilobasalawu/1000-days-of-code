c21bba4c9b2d2e44d67b63466eb56dd9e8c70b8e
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-29 15:27:47 +0100

    add default report to menu data file

--- 100daysOfCode-Angela Yu/Day15/menuData.py
+++ 100daysOfCode-Angela Yu/Day15/menuData.py
@@ -1,4 +1,10 @@
 MENU = {
+    "default" : {
+        'water': 300,
+        'milk': 200,
+        'coffee': 100,
+        'money': 0
+    },
     "espresso": {
         "ingredients": {
             "water": 50,
