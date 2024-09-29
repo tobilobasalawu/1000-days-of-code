60bd20643bc720e861dc5001230556bfe481e517
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-29 15:22:38 +0100

    push menu data into another file

--- 100daysOfCode-Angela Yu/Day15/myCode.py
+++ 100daysOfCode-Angela Yu/Day15/myCode.py
@@ -1,28 +1,3 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
 
 def main():
     userRequest = input("What would you like? (espresso/latte/cappuccino):\n").lower()
