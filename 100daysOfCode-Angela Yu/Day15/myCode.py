d974a8e6d68f3904e2d50955a8a4811780bfaa9b
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-29 15:31:43 +0100

    returning report statement based on users input

--- 100daysOfCode-Angela Yu/Day15/myCode.py
+++ 100daysOfCode-Angela Yu/Day15/myCode.py
@@ -2,3 +2,8 @@ from menuData import MENU
 
 def main():
     userRequest = input("What would you like? (espresso/latte/cappuccino):\n").lower()
+    if userRequest == 'report':
+        print(f"Water: {MENU['default']['water']}ml\nMilk: {MENU['default']['milk']}ml\nCoffee: {MENU['default']['coffee']}g\nMoney: ${MENU['default']['money']}")
+
+
+main()
