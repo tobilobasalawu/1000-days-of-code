74d80fc9e0bdde3a750b800c466bbfcf49f610c5
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-30 17:38:13 +0100

    structured the delete option

--- Extras/adminControl.py
+++ Extras/adminControl.py
@@ -1,9 +1,10 @@
-operation = input("What operation: ").lower()
-addname = input("Enter new username: ").lower()
-addpassowrd = input("Enter new password: ").lower()
-userID = input("The user ID: ").lower()
+operation = ''
+addname = ''
+addpassowrd = ''
+userID = ''
 
 def userManagement(operation, addname, addpassowrd: str) -> str:
+    operation = input("What operation: ").lower()
     userpassword = '242425'
     username = 'aser'
 
@@ -14,12 +15,26 @@ def userManagement(operation, addname, addpassowrd: str) -> str:
         }
     }
 
+    userID = input("The user ID: ").lower()
+
     if operation.lower() == 'add':
+        addname = input("Enter new username: ").lower()
+        addpassowrd = input("Enter new password: ").lower()
         if userID == 'default':
             details['default']['username'] = addname
             details['default']['password'] = addpassowrd
 
             print('New Password and UserName has been set')
+        else:
+            print('Wrong Username')
+
+    if operation.lower() == 'delete':
+        if userID == 'default':
+            details['default']['username'] = None
+            details['default']['password'] = None
+
+            print('New Password and UserName has been deleted')
+            print(details)
         else:
             print('Wrong Username')
 
