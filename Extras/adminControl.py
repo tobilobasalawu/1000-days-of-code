45529b87f4bc4db3eb7f6183e16ddc02e320c99f
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-30 17:27:02 +0100

    structured the add option

--- Extras/adminControl.py
+++ Extras/adminControl.py
@@ -1,34 +1,30 @@
 operation = input("What operation: ").lower()
 addname = input("Enter new username: ").lower()
 addpassowrd = input("Enter new password: ").lower()
-username = input("The user ID ").lower()
+userID = input("The user ID: ").lower()
 
-def userManagement(operation, addname, addpassowrd, username: str) -> str:
+def userManagement(operation, addname, addpassowrd: str) -> str:
     userpassword = '242425'
     username = 'aser'
 
     details = {
-        'userdefault': {
-            'username': addname,
-            'password': addpassowrd
+        'default': {
+            'username': username,
+            'password': userpassword
         }
     }
 
     if operation.lower() == 'add':
-        username = username
-        if username == 'userdefault':
-            addname = addname
-            addpassowrd = addpassowrd
+        if userID == 'default':
+            details['default']['username'] = addname
+            details['default']['password'] = addpassowrd
 
-            details['userdefault']['username'] = addname
-            details['userdefault']['password'] = addpassowrd
-
-            print(details)
+            print('New Password and UserName has been set')
         else:
             print('Wrong Username')
 
 
-userManagement(operation, addname, addpassowrd, username)
+userManagement(operation, addname, addpassowrd)
 
 
 
