f70df684c47a4702f113fce536dba0d9c920e748
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-30 16:55:31 +0100

    adminControl program from school

---
+++ Extras/adminControl.py
@@ -0,0 +1,10 @@
+operation = input("What operation: ")
+addname = input("What the users details name: ")
+addpassowrd = input("What password you want the users: ")
+username = input("What username you want the users: ")
+
+
+
+
+
+
