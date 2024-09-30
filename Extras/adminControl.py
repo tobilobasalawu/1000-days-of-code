51948bb426297a370f4f0b91316c8559e9356c80
John Name <oluwatobilobasalawu1234@gmail.com>
2024-09-30 16:59:36 +0100

    adding the program to a procedure

--- Extras/adminControl.py
+++ Extras/adminControl.py
@@ -4,7 +4,29 @@ addpassowrd = input("What password you want the users: ")
 username = input("What username you want the users: ")
 
 
+def userManagement(operation, addname, addpassowrd, username: str) -> str:
+    userpassword = '242425'
+    username = 'aser'
 
+    details = {
+        'userdefault': {
+            'username': username,
+            'password': userpassword
+        }
+    }
+    if operation.lower() == 'add':
+        username = input('Whats the user name: ')
+        if username == details['userdefault']:
+            addname = input("Enter username: ")
+            addpassowrd = input("Enter password: ")
+
+            details['userdefault']['username'] = addname
+            details['userdefault']['password'] = addpassowrd
+
+            print('Done')
+
+
+userManagement(operation, addname, addpassowrd, username)
 
 
 
