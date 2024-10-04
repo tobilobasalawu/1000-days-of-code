f3dd7439e575fd8823fcc2c08717afac08d68b68
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-04 19:19:20 +0100

    exploring turtle graphics

--- 100daysOfCode-Angela Yu/Day16/main.py
+++ 100daysOfCode-Angela Yu/Day16/main.py
@@ -1,6 +1,16 @@
 from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color('orange')
+timmy.forward(100)
+
+for steps in range(100):
+    for c in ('blue', 'red', 'green'):
+        timmy.color(c)
+        timmy.forward(steps)
+        timmy.right(30)
 
 myScreen = Screen()
 print(myScreen.canvheight)
+myScreen.exitonclick()
