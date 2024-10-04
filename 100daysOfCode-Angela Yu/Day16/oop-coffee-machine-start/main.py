77f5cca01d4caf04ffeae0fc7c0d1cf4f43110cf
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-04 20:42:34 +0100

    dealing with calling objects, methods & attributes

--- 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
+++ 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
@@ -2,3 +2,13 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+name = 'Latte'
+cost = 2.5
+milk = 45
+coffee = 29
+water = 50
+
+items = MenuItem(name, cost, milk, coffee, water)
+print(items)
+
+
