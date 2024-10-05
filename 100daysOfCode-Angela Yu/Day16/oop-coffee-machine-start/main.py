6168bde54e0609dd80881c70d5e2405ca6149fbc
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-05 18:56:47 +0100

    calling the menu list

--- 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
+++ 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
@@ -2,4 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+def Main():
+    coffeeMenu = Menu()
+    print(coffeeMenu.get_items())
 
+Main()
