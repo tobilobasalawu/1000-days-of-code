77a97c0001108310854509c6cda9c0cfdd4908c1
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-05 19:38:20 +0100

    returning total cost and change of user input

--- 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
+++ 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
@@ -3,25 +3,28 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffeeMachine = True
+coffeeMenu = Menu()
+coffeeMoney = MoneyMachine()
+coffeeMake = CoffeeMaker()
 
 def Main():
     coffeeMachine = True
     while coffeeMachine:
-        coffeeMenu = Menu()
         menuInput = input(f"\nWhat would you like? ({coffeeMenu.get_items()}):\n").lower()
-
-        coffeeMoney = MoneyMachine()
-        coffeeMake = CoffeeMaker()
+        change = coffeeMoney.make_payment(coffeeMenu.find_drink(menuInput).cost)
 
         if menuInput == 'report':
             coffeeMoney.report()
             coffeeMake.report()
 
         elif menuInput == 'espresso':
-            coffeeMoney.process_coins()
+            print(change)
 
         elif menuInput == 'quit':
             coffeeMachine = False
             print('\nCoffee Machine, Turned Off')
 
 Main()
+
+
+
