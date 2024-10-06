fb7e93a8cd39fea4a968526376fed1d5995ca841
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-06 15:22:43 +0100

    program completed

--- 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
+++ 100daysOfCode-Angela Yu/Day16/oop-coffee-machine-start/main.py
@@ -1,26 +1,24 @@
-from menu import Menu, MenuItem
+
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-coffeeMachine = True
-coffeeMenu = Menu()
-coffeeMoney = MoneyMachine()
-coffeeMake = CoffeeMaker()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-while coffeeMachine:
-    menuInput = input(f"\nWhat would you like? ({coffeeMenu.get_items()}):\n").lower()
-    if menuInput == 'report':
-        coffeeMoney.report()
-        coffeeMake.report()
+is_on = True
 
-    elif menuInput == 'quit':
-        coffeeMachine = False
-
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = coffeeMenu.find_drink(menuInput)
-        isSufficient = coffeeMake.is_resource_sufficient(drink)
-        isPayment = coffeeMoney.make_payment(drink.cost)
-        if isSufficient and isPayment:
-            #coffeeMoney.process_coins()
-            coffeeMake.make_coffee(drink)
+        drink = menu.find_drink(choice)
 
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
