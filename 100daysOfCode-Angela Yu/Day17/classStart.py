4e8e9c773b96e58542a0ea6e472e8242243712ed
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-06 16:37:41 +0100

    creating the objects

--- 100daysOfCode-Angela Yu/Day17/classStart.py
+++ 100daysOfCode-Angela Yu/Day17/classStart.py
@@ -2,7 +2,11 @@ class User:
     def __init__(self, userId, userName):
         self.id = userId
         self.name = userName
+        self.followers = 0
 
 
 user_1 = User('s45561076', 'Tobi Salawu')
 print(user_1.id, user_1.name)
+
+user_2 = User('s4610859', 'Biology')
+print(user_2.id)
