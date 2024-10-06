ac81ff53bf9c15f56e16c5d050d2c621cc5079fd
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-06 16:54:39 +0100

    creating a method within class

--- 100daysOfCode-Angela Yu/Day17/classStart.py
+++ 100daysOfCode-Angela Yu/Day17/classStart.py
@@ -3,7 +3,11 @@ class User:
         self.id = userId
         self.name = userName
         self.followers = 0
+        self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following +=1
 
 user_1 = User('s45561076', 'Tobi Salawu')
 print(user_1.id, user_1.name)
