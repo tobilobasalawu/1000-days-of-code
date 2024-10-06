405eb2b1c8b80fd36ac8be28222eaf4e8d9034f8
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-06 17:17:09 +0100

    completed first task of creating question

--- 100daysOfCode-Angela Yu/Day17/quizGame/questionModel.py
+++ 100daysOfCode-Angela Yu/Day17/quizGame/questionModel.py
@@ -2,3 +2,6 @@ class Question:
     def __init__(self, question, result):
         self.text = question
         self.answer = result
+
+newQuestion = Question('YES', False)
+print(newQuestion.text)
