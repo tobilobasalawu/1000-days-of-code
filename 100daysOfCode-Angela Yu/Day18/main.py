02377bffab965541c56515098a42e8d86baf94d4
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-11 21:00:20 +0100

    drawing a dashed line

--- 100daysOfCode-Angela Yu/Day18/main.py
+++ 100daysOfCode-Angela Yu/Day18/main.py
@@ -1,11 +1,18 @@
 from turtle import Turtle, Screen
 
-timmyTurtle = Turtle()
-timmyTurtle.shape('turtle')
+turtle = Turtle()
+turtle.shape('turtle')
 
-for i in range(4):
-    timmyTurtle.fd(100)
-    timmyTurtle.lt(90)
+# for i in range(4):
+#     turtle.fd(100)
+#     turtle.lt(90)
+
+
+for i in range(10):
+    turtle.forward(20)
+    turtle.penup()
+    turtle.forward(20)
+    turtle.pendown()
 
 
 
