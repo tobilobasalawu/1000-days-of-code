6f2eabd4477f9ef32a553d448ee6ac629d5ec3a5
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-11 17:50:30 +0100

    drawing a square

--- 100daysOfCode-Angela Yu/Day18/main.py
+++ 100daysOfCode-Angela Yu/Day18/main.py
@@ -1,6 +1,20 @@
 from turtle import Turtle, Screen
 
 timmyTurtle = Turtle()
+timmyTurtle.shape('turtle')
+
+timmyTurtle.fd(100)
+timmyTurtle.lt(90)
+timmyTurtle.fd(100)
+timmyTurtle.lt(90)
+timmyTurtle.fd(100)
+timmyTurtle.lt(90)
+timmyTurtle.fd(100)
+
+
+
+
+
 
 
 
