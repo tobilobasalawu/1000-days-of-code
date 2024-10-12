9ac15037017a02de4a8ea0e642da73b0a7c1e6de
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 13:15:34 +0100

    refactoring the existing program

--- 100daysOfCode-Angela Yu/Day18/main.py
+++ 100daysOfCode-Angela Yu/Day18/main.py
@@ -14,64 +14,12 @@ turtle.shape('turtle')
 #     turtle.forward(20)
 #     turtle.pendown()
 
+def drawShape(noOfSides):
+    turtle.forward(100)
+    turtle.rt(noOfSides)
 
-def square():
-    for i in range(4):
-        turtle.color('blue')
-        turtle.forward(90)
-        turtle.rt(90)
-
-def triangle():
-    turtle.color('red')
-    for i in range(3):
-        turtle.forward(100)
-        turtle.rt(120)
-
-def pentagon():
-    turtle.color('brown')
-    for i in range(5):
-        turtle.forward(100)
-        turtle.rt(72)
-
-def hexagon():
-    turtle.color('green')
-    for i in range(6):
-        turtle.forward(100)
-        turtle.rt(60)
-
-def heptagon():
-    turtle.color('black')
-    for i in range(7):
-        turtle.forward(100)
-        turtle.rt(51.43)
-
-def octagon():
-    turtle.color('dark khaki')
-    for i in range(8):
-        turtle.forward(100)
-        turtle.rt(45)
-
-def nonagon():
-    turtle.color('DarkOrange1')
-    for i in range(9):
-        turtle.forward(100)
-        turtle.rt(40)
-
-def decagon():
-    turtle.color('dark salmon')
-    for i in range(10):
-        turtle.forward(100)
-        turtle.rt(36)
 
 def mainTurtle():
-    square()
-    triangle()
-    pentagon()
-    hexagon()
-    heptagon()
-    octagon()
-    nonagon()
-    decagon()
 
 mainTurtle()
 
