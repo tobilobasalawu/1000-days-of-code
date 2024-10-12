82cff694053b35237b0bb4634ef1a0f08f3d8ea6
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 15:16:31 +0100

    changed the error with random module

--- 100daysOfCode-Angela Yu/Day18/spirograph.py
+++ 100daysOfCode-Angela Yu/Day18/spirograph.py
@@ -6,9 +6,9 @@ txl = t.Turtle()
 t.colormode(255)
 
 def randomColour():
-    r = random.choice(255)
-    g = random.choice(255)
-    b = random.choice(255)
+    r = random.randint(0, 255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
     return (r,g,b)
 
 for i in range(10):
