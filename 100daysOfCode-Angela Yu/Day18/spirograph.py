88b11cdcc499539d56978e1464e7651b7d6e7e88
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 15:11:54 +0100

    defining the color mode using the random colorr func

--- 100daysOfCode-Angela Yu/Day18/spirograph.py
+++ 100daysOfCode-Angela Yu/Day18/spirograph.py
@@ -1,5 +1,23 @@
+import turtle as t
 from turtle import Turtle, Screen
 import random
+
+txl = t.Turtle()
+t.colormode(255)
+
+def randomColour():
+    r = random.choice(255)
+    g = random.choice(255)
+    b = random.choice(255)
+    return (r,g,b)
+
+
+
+
+
+
+
+
 
 
 screen = Screen()
