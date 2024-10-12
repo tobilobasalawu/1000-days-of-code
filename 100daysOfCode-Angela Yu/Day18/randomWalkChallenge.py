19532a9579f799c0d5be54e129a6c472ada6aac3
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 14:46:01 +0100

    refactoring the code

--- 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
+++ 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
@@ -6,28 +6,7 @@ turtle = Turtle()
 color = ["red", 'yellow', 'black', 'blue', 'dark salmon', 'dark sea green', 'DarkGoldenrod1', 'OrangeRed3', 'tomato', 'green', 'dark slate gray']
 pos = [0, 90, 180, 270]
 
-def random1():
-    for i in range(20):
-        turtle.color(random.choice(color))
-        turtle.speed(9)
-        turtle.pensize(10)
-        turtle.forward(30)
-        turtle.lt(random.choice(pos))
 
-
-def random2():
-    for i in range(20):
-        turtle.color(random.choice(color))
-        turtle.speed(9)
-        turtle.pensize(10)
-        turtle.forward(30)
-        turtle.rt(random.choice(pos))
-
-def main():
-    second = [random1(), random2()]
-    random.choice(second)
-
-main()
 
 
 screen = Screen()
