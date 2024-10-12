e5bd12b950b009de3fb051c5b6079ad1fa84f4e6
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 15:03:31 +0100

    creating a func for randomcolour

--- 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
+++ 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
@@ -4,13 +4,15 @@ import random
 Txtle = Turtle()
 turtle.colormode(255)
 
-
+def randomColour():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
 
 pos = [0, 90, 180, 270]
 
-
 for i in range(5000):
-    Txtle.color(random.choice(color))
+    #Txtle.color(random.choice(color))
     Txtle.pensize(10)
     Txtle.speed('fastest')
     Txtle.forward(30)
