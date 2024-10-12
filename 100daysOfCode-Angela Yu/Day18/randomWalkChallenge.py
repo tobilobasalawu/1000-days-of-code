2eb795003aeb8819dbd8a256ed4250164ee5ea2f
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 13:47:10 +0100

    setting the turtle color

--- 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
+++ 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
@@ -1,6 +1,11 @@
 from turtle import Turtle, Screen
 import random
 
+color = ["red", 'yellow', 'black', 'blue', 'dark salmon', 'dark sea green', 'DarkGoldenrod1', 'OrangeRed3', 'tomato', 'green', 'dark slate gray']
+
+turtle = Turtle()
+
+
 
 
 
