9fc4e2e2254140f4d38d80258ed1cf0446c5b28c
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-12 14:35:36 +0100

    creating the second func for drawing

--- 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
+++ 100daysOfCode-Angela Yu/Day18/randomWalkChallenge.py
@@ -1,16 +1,31 @@
 from turtle import Turtle, Screen
 import random
-
-color = ["red", 'yellow', 'black', 'blue', 'dark salmon', 'dark sea green', 'DarkGoldenrod1', 'OrangeRed3', 'tomato', 'green', 'dark slate gray']
-
 turtle = Turtle()
 
-turtle.color(random.choice(color))
-turtle.pensize(10)
-turtle.forward(30)
-turtle.lt(90)
-turtle.forward(30)
-turtle.rt(90)
+
+color = ["red", 'yellow', 'black', 'blue', 'dark salmon', 'dark sea green', 'DarkGoldenrod1', 'OrangeRed3', 'tomato', 'green', 'dark slate gray']
+pos = [90, 180, 360]
+
+def random1():
+    for i in range(20):
+        turtle.color(random.choice(color))
+        turtle.speed(9)
+        turtle.pensize(10)
+        turtle.forward(30)
+        turtle.lt(random.choice(pos))
+
+
+def random2():
+    for i in range(40):
+        turtle.color(random.choice(color))
+        turtle.speed(9)
+        turtle.pensize(10)
+        turtle.forward(30)
+        turtle.rt(random.choice(pos))
+
+
+
+main()
 
 
 screen = Screen()
