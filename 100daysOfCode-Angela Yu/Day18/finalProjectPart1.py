ec1bab2ab387537968c5a015fb3b681e9f02e1a0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-13 17:05:51 +0100

    retrieving each color into tuples

--- 100daysOfCode-Angela Yu/Day18/finalProjectPart1.py
+++ 100daysOfCode-Angela Yu/Day18/finalProjectPart1.py
@@ -1,3 +1,11 @@
 import colorgram
 
 colors = colorgram.extract("hirstPainting.jpg", 16)
+colorStorage = []
+
+for i in colors:
+    r = colors.rgb.r
+    g = colors.rgb.g
+    b = colors.rgb.b
+
+    colorRange = (r, g, b)
