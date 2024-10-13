167baf15d4ffffbd4b28536ee160033ea62ad2b9
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-13 18:35:49 +0100

    drawing dots with different colours

--- 100daysOfCode-Angela Yu/Day18/finalProjectPart2.py
+++ 100daysOfCode-Angela Yu/Day18/finalProjectPart2.py
@@ -1,5 +1,20 @@
 from turtle import Turtle, Screen
-from random import random
+import random
+import turtle
+
 t = Turtle()
+turtle.colormode(255)
 
 colorList = [(243, 242, 239), (171, 158, 33), (99, 6, 51), (75, 94, 173), (232, 209, 73), (10, 35, 127), (178, 104, 155), (215, 89, 34), (105, 123, 210), (26, 96, 40), (33, 103, 47), (242, 237, 240), (113, 131, 212), (184, 116, 161), (218, 92, 40), (232, 235, 244)]
+t.dot(20, random.choice(colorList))
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
