5c863dfb27f58ea90c6b4695d443ebcfd781d83f
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 20:48:21 +0000

    Adding a Label to the program

--- 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -1,6 +1,11 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title('Tkinter GUI')
+window.minsize(width=500, height=400)
 
+
+#label
+myLabel = tkinter.Label(text="This is a label")
 
 window.mainloop()
