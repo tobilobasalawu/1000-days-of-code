5101ab7ff8712f2bf0efb9dbfd0d802e0e2f0428
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 20:49:58 +0000

    setting the label with the pack

--- 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -6,6 +6,7 @@ window.minsize(width=500, height=400)
 
 
 #label
-myLabel = tkinter.Label(text="This is a label")
+myLabel = tkinter.Label(text="This is a label", font=('Arial', 24))
+myLabel.pack()
 
 window.mainloop()
