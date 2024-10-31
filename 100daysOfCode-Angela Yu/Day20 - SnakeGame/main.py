c973e742f4da9a07ac69a20df77937267996a8d7
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 20:24:34 +0000

    using tkinter

--- 100daysOfCode-Angela Yu/Day20 - SnakeGame/main.py
+++
@@ -1,9 +0,0 @@
-from turtle import Screen, Turtle
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
-
-
-screen.exitonclick()
---
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -0,0 +1,6 @@
+import tkinter
+
+window = tkinter.Tk()
+
+
+window.mainloop()
