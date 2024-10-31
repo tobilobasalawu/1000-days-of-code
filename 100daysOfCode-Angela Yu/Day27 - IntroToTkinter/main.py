1ce822e2c4619c0a63921f55c3406fa44ce2afb0
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 22:53:26 +0000

    updating the text of a component using config method

--- 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -10,5 +10,6 @@ myLabel = tkinter.Label(text="This is a label", font=('Arial', 24), )
 myLabel.pack(side='left')
 
 myLabel['text'] = 'New Text' #updating the text of a component by using dict keys
+myLabel.config(text = 'New Text') #updating the text of a component using config method
 
 window.mainloop()
