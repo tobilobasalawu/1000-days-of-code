a2bdb89f0df841be66a83f151a570936485d3961
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 21:45:43 +0000

    *args & **kwargs - learnt something new

--- 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -6,7 +6,7 @@ window.minsize(width=500, height=400)
 
 
 #label
-myLabel = tkinter.Label(text="This is a label", font=('Arial', 24))
-myLabel.pack()
+myLabel = tkinter.Label(text="This is a label", font=('Arial', 24), )
+myLabel.pack(side='left')
 
 window.mainloop()
---
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/playground.py
@@ -0,0 +1,7 @@
+def addNumbers(*args):
+  total = 0
+  for i in args:
+    total += i
+  print(total)
+
+addNumbers(856, 6986)
