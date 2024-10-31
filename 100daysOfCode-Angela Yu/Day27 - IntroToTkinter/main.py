7e70ccddaf7b817a4e46bfead828cdf53bc86af3
John Name <oluwatobilobasalawu1234@gmail.com>
2024-10-31 22:51:28 +0000

    updating the text of a component by using dict keys

--- 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
+++ 100daysOfCode-Angela Yu/Day27 - IntroToTkinter/main.py
@@ -9,4 +9,6 @@ window.minsize(width=500, height=400)
 myLabel = tkinter.Label(text="This is a label", font=('Arial', 24), )
 myLabel.pack(side='left')
 
+myLabel['text'] = 'New Text' #updating the text of a component by using dict keys
+
 window.mainloop()
